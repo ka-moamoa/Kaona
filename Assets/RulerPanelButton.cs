8f9ca1d3e0eb8c033f7c1e6984755ded5bfd0a9e
Michael Parkin <80671326+MikeParkinWits@users.noreply.github.com>
2025-05-13 11:53:51 -0400

    Added Healed Tile State & Ruler Panel Template

--- Assets/RulerPanelButton.cs
+++ Assets/RulerPanelButton.cs
@@ -7,6 +7,11 @@ public class RulerPanelButton : MonoBehaviour
 
     public Animator RulerPanelAnim;
 
+    public Animator FFAnimator;
+
+    public MeshRenderer[] lokahiWheelRenderers = new MeshRenderer[6];
+    public Material[,] lokahiWheelMaterials = new Material[6, 3];
+
     // Start is called before the first frame update
     void Start()
     {
@@ -31,6 +36,12 @@ public class RulerPanelButton : MonoBehaviour
 
     public void HealFFRuler()
     {
-        RulerPanelAnim.Play("OpenFFRuler");
+        RulerPanelAnim.Play("Heal FF Ruler");
+        FFAnimator.Play("FF Turn Healed");
+    }
+
+    public void CloseHealedRuler()
+    {
+        RulerPanelAnim.Play("Close FF Healed Ruler");
     }
 }
